ed841dc371c51217229e7b60677d1bb472532679
Marcel <malbrecht0792@gmail.com>
2018-04-23 14:48:19 -0400

    Moved state to Game.js

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
+import { Setup } from './Setup.js'
 
 class Game extends Component {
-    constructor(props) {
-        super(props);
+
+    constructor() {
+        super();
         this.state = {
+            rounds: "10",
+            newGame: false,
             guesses: []
         }
-        console.log(`Created new game with ${this.props.rounds} rounds`);
+  
+        this.handleOptionChange = this.handleOptionChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+        console.log(`Created new game with ${this.state.rounds} rounds`);
+    }
+  
+    handleOptionChange(selectedOption) {
+        this.setState(
+            {rounds: selectedOption}
+        )
+    }
+  
+    handleFormSubmit() {
+      console.log('You have selected:', this.state.selectedOption);
+      this.setState(
+        {newGame: true}
+      )
     }
 
     render() {
+        let game = <div></div>
+        if(this.state.newGame) {
+            game = <div>{`Created new game with ${this.state.rounds} rounds`}</div>
+        }
         return (
-            <div>{`Created new game with ${this.props.rounds} rounds`}</div>
+            <React.Fragment>
+                <Setup selectedOption={this.state.rounds}
+                       handleOptionChange={this.handleOptionChange}
+                       handleFormSubmit={this.handleFormSubmit} />
+                {game}
+            </React.Fragment>
         )
     }
 }
